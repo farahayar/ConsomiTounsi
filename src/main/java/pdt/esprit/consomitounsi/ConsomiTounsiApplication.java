d3290be7e2ab399a20f4092e8741a50a9b33870f
Rain <55233091+GNRain@users.noreply.github.com>
2022-04-04 09:16:43 +0100

    First push
    Initial Push

---
+++ src/main/java/pdt/esprit/consomitounsi/ConsomiTounsiApplication.java
@@ -0,0 +1,13 @@
+package pdt.esprit.consomitounsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsomiTounsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsomiTounsiApplication.class, args);
+	}
+
+}
