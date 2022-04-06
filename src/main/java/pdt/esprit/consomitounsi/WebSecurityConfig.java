adb4336bf337d7835fcf2a7af8d20860c0789e4b
Rain <55233091+GNRain@users.noreply.github.com>
2022-04-06 10:01:04 +0100

    10:00 A.M 4/6/2022
    Meh

--- src/main/java/pdt/esprit/consomitounsi/WebSecurityConfig.java
+++ src/main/java/pdt/esprit/consomitounsi/WebSecurityConfig.java
@@ -1,42 +1,68 @@
 package pdt.esprit.consomitounsi;
 
-import org.springframework.context.annotation.Bean;
+import javax.servlet.ServletContext;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(final HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/login*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                .loginPage("/login.html")
+                .defaultSuccessUrl("/landingPage.html", true);
+    }
+
+    @Configuration
+    public class SecSecurityConfig {
+        public SecSecurityConfig() {
+            super();
+        }
+    }
+
+    public class AppInitializer implements WebApplicationInitializer {
+
+        @Override
+        public void onStartup(ServletContext sc) {
+
+            AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
+            root.register(SecSecurityConfig.class);
+
+            sc.addListener(new ContextLoaderListener(root));
+
+            sc.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain"))
+                    .addMappingForUrlPatterns(null, false, "/*");
+        }
+    }
+
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
                 .and()
-                .logout()
-                .permitAll();
+                .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
 }
