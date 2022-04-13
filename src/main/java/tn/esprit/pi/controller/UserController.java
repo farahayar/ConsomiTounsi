8247a906781691df8eb9389e8e41cc8a92455984
Rain <55233091+GNRain@users.noreply.github.com>
2022-04-13 01:55:00 +0100

    User Done

--- src/main/java/tn/esprit/pi/controller/UserController.java
+++ src/main/java/tn/esprit/pi/controller/UserController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,19 +15,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tn.esprit.pi.repository.UserRepository;
+
 @RestController
 @RequestMapping("/resourceRootPath")
-class resourceNameController {
+class UserController {
 
-    @Autowired
-    repositoryClassName repository;
+    UserRepository repository;
 
     @GetMapping
-    public ResponseEntity<List<entityClassName>> getAll() {
+    public ResponseEntity<List<UserController>> getAll() {
+        
         try {
-            List<entityClassName> items = new ArrayList<entityClassName>();
-
-            repository.findAll().forEach(items::add);
+            List<UserController> items = new ArrayList<UserController>();
+            
+            repository.findAll();
 
             if (items.isEmpty())
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -40,41 +41,39 @@ class resourceNameController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<entityClassName> getById(@PathVariable("id") entityIdType id) {
-        Optional<entityClassName> existingItemOptional = repository.findById(id);
+    public ResponseEntity<UserController> getById(@PathVariable("id") String id) {
+        Optional<UserRepository> existingItemOptional = repository.findById(id);
 
         if (existingItemOptional.isPresent()) {
-            return new ResponseEntity<>(existingItemOptional.get(), HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping
-    public ResponseEntity<entityClassName> create(@RequestBody entityClassName item) {
+    public ResponseEntity<UserController> create (@RequestBody UserController item) {
         try {
-            entityClassName savedItem = repository.save(item);
-            return new ResponseEntity<>(savedItem, HttpStatus.CREATED);
+            return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
         }
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<entityClassName> update(@PathVariable("id") entityIdType id, @RequestBody entityClassName item) {
-        Optional<entityClassName> existingItemOptional = repository.findById(id);
+    public ResponseEntity<UserController> update(@PathVariable("id") String id, @RequestBody UserController item) {
+        Optional<UserRepository> existingItemOptional = repository.findById(id);
         if (existingItemOptional.isPresent()) {
-            entityClassName existingItem = existingItemOptional.get();
             System.out.println("TODO for developer - update logic is unique to entity and must be implemented manually.");
             //existingItem.setSomeField(item.getSomeField());
-            return new ResponseEntity<>(repository.save(existingItem), HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<HttpStatus> delete(@PathVariable("id") entityIdType id) {
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") String id) {
         try {
             repository.deleteById(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
